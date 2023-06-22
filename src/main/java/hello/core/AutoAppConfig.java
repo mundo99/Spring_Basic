907d7e8eb69f55098b369970063e2c82055e2bb2
mundo99 <ansehdus7@gmail.com>
2023-06-22 11:45:51 +0900

    탐색 위치와 기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Component;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core.member", // 이 경로 하위 패키지부터 찾아감
+        // 지정 안하면? 디폴트 -> 이게 위치한 경로부터 하위 패키지 (hello.core.*)
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
